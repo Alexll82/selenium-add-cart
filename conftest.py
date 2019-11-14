00fa697e8d8756f352b082adc5cf5a7ff04cf523
Alexll82 <Litv456@yandex.ru>
2019-11-14 22:27:25 +0700

    uploading files

---
+++ conftest.py
@@ -0,0 +1,23 @@
+import pytest 
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options   #Опции для языка
+
+#Язык для Хрома
+options = Options()                                    
+options.add_experimental_option('prefs', {'intl.accept_languages': 'en'}) 
+
+# Обработчик параметров командной строки
+def pytest_addoption(parser):
+    parser.addoption('--language',
+                     action='store',
+                     default="en",
+                     help="Choose language")    
+
+@pytest.fixture(scope="function")               # используем для всех тестов
+def browser(request):
+        language = request.config.getoption("language")
+        options.add_experimental_option('prefs', {'intl.accept_languages': language}) 
+        browser = webdriver.Chrome(options=options)
+        yield browser
+        print("\nquit browser..")
+        browser.quit()
