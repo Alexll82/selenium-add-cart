64e49ae993829df12dad77d78a7890062b8ccf22
Alexll82 <Litv456@yandex.ru>
2019-11-15 23:03:10 +0700

    commented pause

--- test_items.py
+++ test_items.py
@@ -2,6 +2,6 @@ import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/" 
 def test_add_to_cart(browser): 
 	browser.get(link)
-	time.sleep(30)
+	#time.sleep(30)
 	cart = browser.find_elements_by_class_name("btn-add-to-basket")
 	assert len(cart) > 0, "Cart not found"
